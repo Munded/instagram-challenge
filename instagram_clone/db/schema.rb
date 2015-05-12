a4c2ef8db73bd68239dbf6c97a3a5cb77acceb42
Ed O'Brien <edmund.obrien.2010@live.rhul.ac.uk>
2015-05-12 18:52:49 +0100

    basic comment functionality sorted

--- instagram_clone/db/schema.rb
+++ instagram_clone/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512174504) do
+ActiveRecord::Schema.define(version: 20150512174938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20150512174504) do
     t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "post_id"
   end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -30,4 +33,5 @@ ActiveRecord::Schema.define(version: 20150512174504) do
     t.text     "description"
   end
 
+  add_foreign_key "comments", "posts"
 end

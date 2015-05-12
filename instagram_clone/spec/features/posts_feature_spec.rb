2e0568fe36eb568818c280b9c552b60c2bb876f2
Ed O'Brien <edmund.obrien.2010@live.rhul.ac.uk>
2015-05-12 18:17:03 +0100

    Crud sorted

--- instagram_clone/spec/features/posts_feature_spec.rb
+++ instagram_clone/spec/features/posts_feature_spec.rb
@@ -8,4 +8,53 @@ feature 'posts' do
       expect(page).to have_link 'Submit a Post'
     end
   end
+
+  context 'creating posts' do
+    scenario 'prompts user to fill out a form, then displays the new post' do
+      visit '/posts'
+      click_link 'Submit a Post'
+      fill_in 'Name', with: 'Chin'
+      fill_in 'Image', with: 'https://lh3.googleusercontent.com/-0wOhLe6FaEc/AAAAAAAAAAI/AAAAAAAAACU/dscpJHyiBNM/photo.jpg'
+      click_button 'Submit Post'
+      expect(page).to have_content 'Chin'
+      expect(page).to have_xpath("//img[@src='https://lh3.googleusercontent.com/-0wOhLe6FaEc/AAAAAAAAAAI/AAAAAAAAACU/dscpJHyiBNM/photo.jpg']")
+      expect(current_path).to eq '/posts'
+    end
+  end
+
+  context 'posts have been added' do
+
+    before do
+      Post.create(name: 'Chin', image: 'https://lh3.googleusercontent.com/-0wOhLe6FaEc/AAAAAAAAAAI/AAAAAAAAACU/dscpJHyiBNM/photo.jpg')
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content('Chin')
+      expect(page).to have_xpath("//img[@src='https://lh3.googleusercontent.com/-0wOhLe6FaEc/AAAAAAAAAAI/AAAAAAAAACU/dscpJHyiBNM/photo.jpg']")
+      expect(page).not_to have_content('No Posts yet')
+    end
+
+    scenario 'let a user edit a post' do
+     visit '/posts'
+     click_link 'Edit Chin'
+     fill_in 'Name', with: 'Chinzzz'
+     click_button 'Update Post'
+     expect(page).to have_content 'Chinzzz'
+     expect(current_path).to eq '/posts'
+    end
+  end
+
+ context 'viewing restaurants' do
+
+    let!(:chin){Post.create(name:'Chin', image: 'https://lh3.googleusercontent.com/-0wOhLe6FaEc/AAAAAAAAAAI/AAAAAAAAACU/dscpJHyiBNM/photo.jpg')}
+
+    scenario 'lets a user view a post' do
+     visit '/posts'
+     click_link 'Chin'
+     expect(page).to have_content 'Chin'
+     expect(current_path).to eq "/posts/#{chin.id}"
+    end
+  end
+
 end

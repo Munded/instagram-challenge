7a11fcabcd1e65ae40f92067a5ca20dfe035bba4
Ed O'Brien <edmund.obrien.2010@live.rhul.ac.uk>
2015-05-12 21:36:18 +0100

    can only edit and delete own comments

--- instagram_clone/app/controllers/comments_controller.rb
+++ instagram_clone/app/controllers/comments_controller.rb
@@ -6,12 +6,45 @@ class CommentsController < ApplicationController
     @comment = Comment.new
   end
 
+  def show
+    @post = Post.find(params[:post_id])
+    @comment = @post.comment.find(params[:id])
+  end
+
+
   def create
     @post = Post.find(params[:post_id])
     comment = @post.comments.new(comment_params)
     comment.user = current_user
     comment.save
     redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if current_user == @comment.user
+      @comment.update(comment_params)
+      flash[:notice] = 'comment edited successfully'
+    else 
+      flash[:notice] = 'Cannot edit comment'
+    end
+    redirect_to '/posts'
+  end
+
+  def destroy
+     @comment = Comment.find(params[:id])
+    if current_user == @comment.user
+      @comment.destroy
+      flash[:notice] = 'Comment deleted successfully'
+    else
+      flash[:notice] = 'Cannot delete comment'
+    end
+    redirect_to '/posts'
   end
 
   def comment_params
--- instagram_clone/spec/features/comments_feature_spec.rb
+++ instagram_clone/spec/features/comments_feature_spec.rb
@@ -29,4 +29,57 @@ feature 'reviewing' do
     expect(page).to have_content 'You need to sign in or sign up before continuing'
   end
 
+  context 'already created comment' do
+    before do
+      leave_comment
+    end
+
+    scenario 'can edit comment if created' do
+      visit '/posts'
+      click_link 'Edit Comment'
+      fill_in 'Message', with: 'ew'
+      click_link 'Update Comment'
+      expect(page).to have_content 'ew'
+    end
+
+    scenario 'cannot edit comment if not signed it' do
+      click_link 'Sign out'
+      visit '/posts'
+      click_link 'Edit Comment'
+      expect(current_path).to eq '/users/sign_in'
+      expect(page).to have_content 'You need to sign in or sign up before continuing'
+    end
+
+    scenario 'cannot edit comment if did not create' do
+      click_link 'Sign out'
+      sign_up('test2@test.com', '12345678')
+      visit '/posts'
+      click_link 'Edit Comment'
+      fill_in 'Message', with: 'ew'
+      click_link 'Update Comment'
+      expect(page).to have_content 'Cannot edit comment'
+    end
+
+
+    scenario 'can delete comment if created' do
+      visit '/posts'
+      click_link 'Delete Comment'
+      expect(page).not_to have_content 'Cool'
+    end
+
+    scenario 'cannot delete comment if not signed it' do
+      visit '/posts'
+      click_link 'Delete Comment'
+      expect(current_path).to eq '/users/sign_in'
+      expect(page).to have_content 'You need to sign in or sign up before continuing'
+    end
+
+    scenario 'cannot delete comment if did not create' do
+      click_link 'Sign out'
+      sign_up('test2@test.com', '12345678')
+      click_link 'Delete Comment'
+      expect(page).to have_content 'Cool'
+      expect(page).to have_content 'Cannot delete comment'
+    end
+  end
 end

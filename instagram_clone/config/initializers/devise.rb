da6f430dde854d4e69ed64304a3ff98c8afabc8b
Ed O'Brien <edmund.obrien.2010@live.rhul.ac.uk>
2015-05-16 10:13:26 +0100

    facebook secrets sorted

--- instagram_clone/config/initializers/devise.rb
+++ instagram_clone/config/initializers/devise.rb
@@ -257,6 +257,6 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  config.omniauth :facebook, "APP_ID", "APP_SECRET"
-  
+  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret
+
 end
